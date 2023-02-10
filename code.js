98af2756180f83e8d46af7c04e6d2dff5c7d569f
Karl Jiang <karl@figma.com>
2023-02-10 15:50:30 -0800

    change

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+  // it will then create that many rectangles on the screen.
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser enviroment (see documentation).
